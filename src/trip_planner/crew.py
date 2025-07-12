822ff42727d1e06a19e3f80ce0bd06fcce948760
Mourad <hamzaoui.mourad@outlook.com>
2025-07-12 15:30:22 -0400

    Configure Crew - Update crew.py

--- src/trip_planner/crew.py
+++ src/trip_planner/crew.py
@@ -2,63 +2,65 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from trip_planner.tools.custom_tool import SearchTools,BrowserTools,CalculatorTools
+
 
 @CrewBase
-class TripPlanner():
-    """TripPlanner crew"""
+class TripPlannerCrew():
+    """Research crew for comprehensive topic analysis and reporting"""
 
     agents: List[BaseAgent]
     tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+    @agent
+    def city_selection_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['city_selection_agent'], 
+            verbose=True,
+            tools = [SearchTools(), BrowserTools()]
+        )
+
+    @agent
+    def local_expert(self) -> Agent:
+        return Agent(
+            config=self.agents_config['local_expert'], 
+            verbose=True,
+            tools = [SearchTools(), BrowserTools()]
+        )
     
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
-    def researcher(self) -> Agent:
+    def travel_concierge(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
+            config=self.agents_config['travel_concierge'],
             verbose=True
         )
 
-    @agent
-    def reporting_analyst(self) -> Agent:
-        return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
-            verbose=True
-        )
-
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
     @task
-    def research_task(self) -> Task:
+    def identify_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+            config=self.tasks_config['identify_task'],
+            tools = [SearchTools(), BrowserTools(),CalculatorTools()]
         )
 
     @task
-    def reporting_task(self) -> Task:
+    def gather_task(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
+            config=self.tasks_config['gather_task'], 
+            output_file='output/report.md'
+        )
+    @task
+    def plan_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['plan_task'],
+            output_file='output/report.md'
         )
 
     @crew
     def crew(self) -> Crew:
-        """Creates the TripPlanner crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
+        """Creates the trip plan crew"""
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
